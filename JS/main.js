ae68aa3718ed1be17e09c5b2e7a24e53da4f5be3
asif <mhamudasif2@gmail.com>
2021-06-17 09:38:48 +0600

    Quotes added

---
+++ JS/main.js
@@ -0,0 +1,40 @@
+    const getClass =(cls)=>{
+        return document.getElementsByClassName(`${cls}`)
+    }
+    const getId =(id)=>{
+        return document.getElementById(`${id}`)
+    }
+    const fetchQuote =async(url)=>{
+        try{
+            let data = await fetch(url)
+             data = await data.json()
+            updateData(data)
+        }
+        catch(err){
+            return err.message
+        }
+    }
+    const updateData=(data)=>{
+        getId('quotes').innerHTML= JSON.stringify(data.description)
+        console.log(data)
+        
+    }
+    const ran=(limit)=>{
+         return Math.floor(Math.random() * limit).toString()
+    }
+    
+    // main.js
+document.addEventListener('DOMContentLoaded', function () {
+
+                fetchQuote(`https://bn-hadith-api.herokuapp.com/hadiths/${ran(60)}`)
+
+
+
+
+
+
+
+
+
+
+})
