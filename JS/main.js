eab13de80f9cdf84da8fc80dcd54eee68f60d839
asif <mhamudasif2@gmail.com>
2021-06-17 12:28:36 +0600

    fxd

--- JS/main.js
+++ JS/main.js
@@ -1,32 +1,32 @@
-    const getClass =(cls)=>{
-        return document.getElementsByClassName(`${cls}`)
+const getClass =(cls)=>{
+    return document.getElementsByClassName(`${cls}`)
+}
+const getId =(id)=>{
+    return document.getElementById(`${id}`)
+}
+const fetchQuote =async(url)=>{
+    try{
+        let data = await fetch(url)
+         data = await data.json()
+        updateData(data)
     }
-    const getId =(id)=>{
-        return document.getElementById(`${id}`)
+    catch(err){
+        return err.message
     }
-    const fetchQuote =async(url)=>{
-        try{
-            let data = await fetch(url)
-             data = await data.json()
-            updateData(data)
-        }
-        catch(err){
-            return err.message
-        }
-    }
-    const updateData=(data)=>{
-        getId('quotes').innerHTML= JSON.stringify(data.description)
-        console.log(data)
-        
-    }
-    const ran=(limit)=>{
-         return Math.floor(Math.random() * limit).toString()
-    }
+}
+const updateData=(data)=>{
+    getId('quotes').innerHTML= JSON.stringify(data.description)
+    console.log(data)
     
-    // main.js
+}
+const ran=(limit)=>{
+     return Math.floor(Math.random() * limit).toString()
+}
+
+// main.js
 document.addEventListener('DOMContentLoaded', function () {
 
-                fetchQuote(`https://bn-hadith-api.herokuapp.com/hadiths/${ran(60)}`)
+            fetchQuote(`https://bn-hadith-api.herokuapp.com/hadiths/${ran(60)}`)
 
 
 
